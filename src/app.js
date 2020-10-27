b0756745e18b278af1b17e66be0905a6a6681942
sarahelhawary <sarah.elhwarii92@gmail.com>
2020-10-27 19:35:41 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export { Home } from './components'
+export { About } from './components'
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
+  )
+}
